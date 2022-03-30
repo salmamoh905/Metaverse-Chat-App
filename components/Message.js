b29881c8680d5e0ae9e296d645d6114f671b4965
salmamoh905 <samiramoh246@gmail.com>
2022-03-30 21:41:28 +0300

    Chat functionality and saving it in the Database completed

---
+++ components/Message.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useMoralis } from 'react-moralis'
+import Avatar from './Avatar';
+
+function Message({message}) {
+    const {user}=useMoralis();
+
+    const isUserMessage = message.get("ethAddress")=== user.get("ethAddress")
+  return (
+      <div className={`flex items-end space-x-2 relative ${
+          isUserMessage && "justify-end"
+      }`}>
+
+          <div className={`relative h-8 w-8 ${
+              isUserMessage && "order-last"
+          }`}>
+              <Avatar username={message.get("username")}/>
+
+          </div>
+
+
+          <div className={`flex space-x-4 p-3 rounded-lg${
+              isUserMessage? "rounded-br-none bg-slate-300"
+              :"rounded-bl-none bg-green-300"
+          }`}>
+
+          <p>{message.get("message")}</p>
+          </div>
+      </div>
+    
+  )
+}
+
+export default Message
